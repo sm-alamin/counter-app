c3472f2d579f5b5826dcf24a2f70e51cf143542b
sm-alamin <sma.fkml@gmail.com>
2022-04-02 12:57:23 +0600

    finally, counter app is ready

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+import { useState } from 'react';
 import './App.css';
 
 
 function App() {
+  const [count, setCount] = useState(20);
+
   return (
     <div className="App">
       <h1>Counter App</h1>
-      <h2>My Age is:  20</h2>
-      <button className='button1'>+</button>
-      <button className='button2'>-</button>
+      <h2>My Age is:  {count}</h2>
+      <button className='button1' onClick={ ()=>{
+          setCount(count +1);
+      }}>+</button>
+      <button className='button2' onClick={ ()=>{
+          setCount(count -1);
+      }}>-</button>
       
       
     </div>
